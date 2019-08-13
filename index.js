01b8483a6df5775e5e9e6486b1626acee0077394
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-13 21:13:12 +0900

    add passport

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access tolen', accessToken);
+      console.log('refresh tolen', refreshToken);
+      console.log('profile tolen', profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there" });
+// });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
