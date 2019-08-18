9323d2c50f90bef9bf6f85ee097ed6250498d2f8
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-18 23:17:06 +0900

    tweak google strategy setting

--- services/passport.js
+++ services/passport.js
@@ -13,6 +13,7 @@ passport.deserializeUser((id, done) => {
     done(null, user);
   });
 });
+
 // http://localhost:5000/auth/google/
 // http://localhost:5000/api/current_user/
 
@@ -21,7 +22,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
