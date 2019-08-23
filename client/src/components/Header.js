9ad4b22f5ff1b1848cae7d38338e97c20237dfdd
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-23 16:46:31 +0900

    add auth login at header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -9,21 +10,27 @@ class Header extends Component {
       case false:
         return (
           <li>
-            <a href="/auth/google">
-              Login With Google
-            </a>
+            <a href="/auth/google">Login With Google</a>
           </li>
         );
-
       default:
-        return <li><a>Logout</a></li>;
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
     }
   }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
