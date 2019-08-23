d9c87e81139b5b3413f869ec8e0063e9d260f3a6
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-23 19:57:38 +0900

    add stripe

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
     app.use(proxy(
-        ['/api/*', '/auth/google'], 
+        ['/api', '/auth/google'], 
         { target: 'http://localhost:5000' }
     ));
 }
