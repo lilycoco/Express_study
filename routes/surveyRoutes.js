e57295a29d92452551213a6bd199e84fdbf2450c
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-26 13:58:58 +0900

    add email template

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,6 +6,10 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
@@ -22,7 +26,7 @@ module.exports = app => {
     try {
       await mailer.send();
       await survey.save();
-      req.user.credits = -1;
+      req.user.credits = +1;
       const user = await req.user.save();
       res.send(user);
     } catch (err) {
