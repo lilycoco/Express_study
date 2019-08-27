24700e05ed6a60e8419088f7544ba5699512e192
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-27 18:00:38 +0900

    add emailregex

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
   {
@@ -56,6 +57,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || "");
 
   _.each(FIELDS, ({ name, noValueError }) => {
     if (!values[name]) {
