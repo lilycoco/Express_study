0b201ca71eb627dc00916a127f372bfd57e9c7b0
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-28 11:16:12 +0900

    add sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,7 +12,7 @@ module.exports = app => {
 
   app.post("/api/surveys/webhooks", (req, res) => {
     console.log(req.body);
-    res.send({})
+    res.send({});
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
