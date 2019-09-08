0817d3a48c19280ae0669737ee5c518ef3ed335a
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-09-08 19:31:51 +0900

    replace js to ts

---
+++ models/Survey.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose'
+import recipientSchema from './Recipient'
+
+const surveySchema = new Schema({
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [recipientSchema],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateSent: Date,
+  lastResponded: Date,
+})
+
+export default mongoose.model('surveys', surveySchema)
