a7d43369428ef3f63d0693798f8c16edbd780dcd
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-09-15 15:00:07 +0900

    fix proxyMidleware

--- client/src/setupProxy.ts
+++
@@ -1,8 +0,0 @@
-import proxy from 'http-proxy-middleware'
-
-export default function(app: any) {
-    app.use(proxy(
-        ['/api', '/auth/google'], 
-        { target: 'http://localhost:5000' }
-    ));
-}
